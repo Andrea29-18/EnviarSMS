3a9090df1032a796087f89ec7c8ea0e862d1b02f
Andrea29-18 <aavp0316@gmail.com>
2024-09-09 14:06:20 -0600

    Algunos puntos negativos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-
+const {whatsapp} = require('./lib/whatsapp');
 const app = express();
 
 const puerto = 3003;
 
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
 
 //Rutas
 app.use('./api', require('./routes/links'));
 
+whatsapp.initialize();
+
 app.listen(puerto, () => {
     console.log(`Servidor corriendo en el puerto ${puerto}`);
-} 
-);
+});
